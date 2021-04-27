5cdadbe37d3a6d05b259448f58458cd6e0529ebe
Guilherme Mattos <45467253+guilhermemattos16@users.noreply.github.com>
2021-04-27 11:48:08 -0300

    Page creation

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
-Route::resource('/classes', [SchoolClassController::class]);
+Route::get('/aluno', function () {
+    return view('student.detalhes');
+})->name('aluno');
+
+Route::get('/turma', function () {
+    return view('turmas.listar');
+})->name('turma');
+
+Route::get('/turma/detalhes', function () {
+    return view('turmas.detalhes');
+})->name('turma/detalhes');
+
+Route::resource('/classes', SchoolClassController::class);
